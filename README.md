7b5affce59cbd6b89c440bbd138719453f3bf728
Long9725 <develop9725@gmail.com>
2023-11-18 02:45:34 +0900

    <CHORE> Update README.md

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # nginx-rtmp
 Nginx rtmp 모듈입니다. 
 
+## 의존성
+
+### bash
+```
+nginx
+nginx-rtmp-module
+python3 
+python3-pip 
+inotify-tools
+```
+
+### python
+```
+minio
+```
+
 ## ENV
 ```sh
 ENV MINIO_BUCKET_NAME=your-bucket-name 
@@ -8,3 +24,24 @@ ENV MINIO_ACCESS_KEY=your-access-key
 ENV MINIO_SECRET_KEY=your-secret-key 
 ENV MINIO_SERVER=your-mino-server
 ```
+
+## 라이브 인증
+```sh
+on_publish http://instream-tenant-server:8080/api;
+```
+
+nginx 설정 중 위 부분에서 아래와 같이 POST 요청을 보냅니다. 
+
+```sh
+call=play
+addr - client IP address
+clientid - nginx client id (displayed in log and stat)
+app - application name
+flashVer - client flash version
+swfUrl - client swf url
+tcUrl - tcUrl
+pageUrl - client page url
+name - stream name
+```
+
+2xx가 응답되면 publish를 시작합니다.
--- upload_hls_to_minio.py
+++ upload_hls_to_minio.py
@@ -1,24 +1,25 @@
 import os
+import sys
 from minio import Minio
 from minio.error import S3Error
 
-def upload_to_minio(file_path, bucket_name, object_name):
-    client = Minio(
-        'minio-server:9000',
-        access_key='your-access-key',
-        secret_key='your-secret-key',
+def upload_file_to_minio(bucket_name, file_path, minio_client):
+    object_name = os.path.basename(file_path)
+    try:
+        minio_client.fput_object(bucket_name, object_name, file_path)
+        print(f"Uploaded {file_path} to MinIO bucket {bucket_name}/{object_name}")
+    except S3Error as e:
+        print(f"Failed to upload {file_path}: {e}")
+
+if __name__ == "__main__":
+    bucket_name = sys.argv[1] # MinIO 버킷 이름
+    file_path = sys.argv[2]   # 업로드할 파일의 경로
+
+    minio_client = Minio(
+        os.environ['MINIO_SERVER'],
+        access_key=os.environ['MINIO_ACCESS_KEY'],
+        secret_key=os.environ['MINIO_SECRET_KEY'],
         secure=False
     )
 
-    try:
-        client.fput_object(bucket_name, object_name, file_path)
-        print(f"Uploaded {file_path} to {bucket_name}/{object_name}")
-    except S3Error as e:
-        print("MinIO upload failed:", e)
-
-# 예제 파일 경로 및 버킷 이름
-file_path = "/path/to/hls/file.ts"
-bucket_name = "your-bucket"
-object_name = os.path.basename(file_path)
-
-upload_to_minio(file_path, bucket_name, object_name)
+    upload_file_to_minio(bucket_name, file_path, minio_client)
