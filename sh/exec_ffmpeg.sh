a3312448f8ba93e2877b8e23ea3a5faeeb57e463
Long9725 <develop9725@gmail.com>
2023-11-23 15:43:48 +0900

    <REFACTOR> latency down

--- sh/exec_ffmpeg.sh
+++ sh/exec_ffmpeg.sh
@@ -6,9 +6,9 @@ name=$1
 if ps aux | grep -q "[f]fmpeg.*$name"; then
  # ffmpeg 스트리밍 명령 실행
     ffmpeg -i rtmp://localhost/instream-live/$name \
-        -c:a aac -b:a 32k  -c:v libx264 -b:v 128K -f flv rtmp://localhost/hls/${name}_360 \
-        -c:a aac -b:a 64k  -c:v libx264 -b:v 256k -f flv rtmp://localhost/hls/${name}_720 \
-        -c:a aac -b:a 128k -c:v libx264 -b:v 512K -f flv rtmp://localhost/hls/${name}_1080 
+        -vf "scale=640x360" -c:a aac -b:a 32k  -c:v libx264 -g 30 -b:v 128K -f flv rtmp://localhost/hls/${name}_360 \
+        -vf "scale=1280x720" -c:a aac -b:a 64k  -c:v libx264 -g 30 -b:v 256k -f flv rtmp://localhost/hls/${name}_720 \
+        -vf "scale=1920x1080" -c:a aac -b:a 128k -c:v libx264 -g 30 -b:v 512K -f flv rtmp://localhost/hls/${name}_1080 
 else
     echo "exec_ffmpeg.sh: Stream for $name is already running."
 fi
